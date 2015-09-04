82b9a673946e5872588a400ec301ad92c0ea50c1
woshibala <songyang119@126.com>
2015-09-04 10:41:29 -0600

    fix the delete function

--- bt.py
+++ bt.py
@@ -44,37 +44,41 @@ class Binary_tree:
         	    self.__preorder_search(current.right_child,goal)
 
     def delete(self,value):#pre-order
-        current = self.preorder_search(self.root,value)
-        if current == None:
+        self.__preorder_search(self.root,value)
+        if self.search_result.key == value:
+            current = self.search_result
+            #if the node has two children 
+            if current.left_child != None and current.right_child != None:
+                print("Node not deleted, has two children")
+            #if the node has no child
+            if current.left_child == None and current.right_child == None:
+                if current.parent.left_child == current.key:#if current node is the left child of its parent node
+                    current.parent.left_child = None
+                    print("Node deleted")
+                else:#if current node is the right child of its parent node
+                    current.parent.right_child = None
+                    print("Node deleted")
+                return
+            #if the node has only one child
+            print("Node not deleted, has one child")
+        else:
             print("Node not found.")
-        #if the node has two children 
-        if current.left_child != None and current.right_child != None:
-            print("Node not deleted, has two children")
-        #if the node has no child
-        if current.left_child == None and current.right_child == None:
-            if current.parent.left_child == current.key:#if current node is the left child of its parent node
-                current.parent.left_child = None
-            else:#if current node is the right child of its parent node
-                current.parent.right_child = None
-            return
-        #if the node has only one child
-        print("Node not deleted, has one child")
-        #try two delete the node that has only one child & it works
-        '''if current.left_child == None:
-            if current.parent.left_child.key == current.key:#the node is the left child of its parent node
-                current.right_child.parent = current.parent
-                current.parent.left_child = current.right_child
-            else:#the node is the right child of its parent node
-                current.right_child.parent = current.parent
-                current.parent.right_child = current.right_child
-        else:# current.right_child == None:
-            if current.parent.left_child.key == current.key:#the node is the left child of its parent node
-                current.left_child.parent = current.parent
-                current.parent.left_child = current.left_child
-            else:#the node is the right child of its parent node
-                current.left_child.parent = current.parent
-                current.parent.right_child = current.left_child
-        return''' 
+            #try two delete the node that has only one child & it works
+            '''if current.left_child == None:
+                if current.parent.left_child.key == current.key:#the node is the left child of its parent node
+                    current.right_child.parent = current.parent
+                    current.parent.left_child = current.right_child
+                else:#the node is the right child of its parent node
+                    current.right_child.parent = current.parent
+                    current.parent.right_child = current.right_child
+                else:# current.right_child == None:
+                if current.parent.left_child.key == current.key:#the node is the left child of its parent node
+                    current.left_child.parent = current.parent
+                    current.parent.left_child = current.left_child
+                else:#the node is the right child of its parent node
+                    current.left_child.parent = current.parent
+                    current.parent.right_child = current.left_child
+            return''' 
     def print_preorder(self,current):#pre-order traversal & print the tree
         if current == None:
         	return None
@@ -87,10 +91,13 @@ t = Binary_tree(1)
 t.add(2,1)
 t.add(3,1)
 t.Print()
+print("*********")
 t.add(4,2)
 t.add(5,2)
 t.Print()
-
+print("*********")
+t.delete(5)
+t.delete(2)
 
 #                1
 #             2     3
